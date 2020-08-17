f404ed16ca231cecc14f13a881d08de2e0fa78e6
ngoclap585 <ngoclap858@gmail.com>
2020-08-17 20:20:00 +0700

    Chinh Sua Model

--- app/Street.php
+++ app/Street.php
@@ -9,16 +9,13 @@ class Street extends Model
     protected $table ='Street';
     protected $primaryKey = 'StreetId';
     protected $guarded = [];
-<<<<<<< HEAD
-
     
-}
-=======
     protected $fillable = ['AreaId', 'Name', 'Status'];
 
     public function posts()
      {
          return $this->hasMany('App\Post', 'StreetId', 'StreetId');
      }
+   
 }
->>>>>>> 950439005aeb7d1a88eb41a7147f0937bccf670d
+
