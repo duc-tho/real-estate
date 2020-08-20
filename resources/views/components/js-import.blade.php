efc6d06fce691d799b0f428362bd2e114b863756
Ngô Đức Thọ <45930156+3ei3isaki@users.noreply.github.com>
2020-08-21 00:24:53 +0700

    Create Login and Register

---
+++ resources/views/components/js-import.blade.php
@@ -0,0 +1,4 @@
+{{-- Plugins --}}
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
