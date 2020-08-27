d9a750a89d62aca4aea97d546767d8c7fee59bf6
ngoclap585 <ngoclap858@gmail.com>
2020-08-27 13:54:26 +0700

    Conflict, note lại script

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,11 +17,6 @@
      @yield('main')
      @include('components.js-import')
      @yield('js-import')
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> c971faf9278ae05bacd343c65e65e4d1aa46c944
 </body>
 
 </html>
