df7c62e94f3ceaaf1b838b63d622b37b1601c8c0
Ngô Đức Thọ <45930156+3ei3isaki@users.noreply.github.com>
2020-11-03 17:17:59 +0700

    Add gradient in district image

--- resources/views/Components/district.blade.php
+++ resources/views/Components/district.blade.php
@@ -1,7 +1,6 @@
-<div class="item itemarea px-1">
+<div class="district-image item itemarea px-1">
      <a href="{{ $data->url }}">
-          {{-- <img src="{{ asset('dist/img/los-angeles-winter-2016-410x270.jpg')}}" alt="Alhambra"> --}}
-          <img src="{{ asset($data->Image)}}" alt="{{ $data->Name }}">
+          <img class="district-image" src="{{ asset($data->Image ?? 'dist/img/los-angeles-winter-2016-410x270.jpg')}}" alt="{{ $data->Name }}">
           <h4>{{ $data->Name }}</h4>
      </a>
 </div>
