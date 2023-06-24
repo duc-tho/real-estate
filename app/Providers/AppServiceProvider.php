a3aae0f5ca7d9d908da0b8fca34a318903ffae88
Ngô Đức Thọ <45930156+duc-tho@users.noreply.github.com>
2023-06-24 06:56:12 +0000

    Fix devcontainer on code space

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Setting;
 use App\Observers\SettingObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Setting::observe(SettingObserver::class);
+        URL::forceScheme('https');
     }
 }
